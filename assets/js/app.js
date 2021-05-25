8af939d9fb206373eeeb9984e7340c38afd60bc7
paulinegallezot <paulinegallezot@gmail.com>
2021-05-25 09:15:10 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,13 @@ let productsEl = document.getElementById('cardsProducts');
 fetch('/assets/data/products.json')
 .then((response) => response.json()) 
 .then((displayCards) => {
-    displayCards.results.forEach((element) => {
+    displayCards.products.forEach((element) => {
     productsEl.innerHTML += `<div class="col-4 card" style="width: 25rem;height:42rem;"><div class="">
-    <img src="${element.poster_path}" class="card-img-top "></div>
+    <img src="${element.img_src}" class="card-img-top "></div>
     <div class="card-body color">
-      <h5 class="card-title">${element.original_title}</h5>
-      <p class="card-text">${element.overview}</p>
-      <button class="">ajouter au panier</button>
+      <h5 class="card-title">${element.title}</h5>
+      <p class="card-text">${element.desc}</p>
+      <button class="btnColor">ajouter au panier</button>
     </div>
   </div>`;
    });
