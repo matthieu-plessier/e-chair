beb0b02f70cdda5b404c1f3e3a9c217a78a65775
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-25 14:35:34 +0200

    PANIER

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,68 @@
 let productsEl = document.getElementById('cardsProducts');
+var collectionCart = [];
+var nullCart = true;
+
 fetch('/assets/data/products.json')
-.then((response) => response.json()) 
-.then((displayCards) => {
+  .then((response) => response.json())
+  .then((displayCards) => {
     displayCards.products.forEach((element) => {
-    productsEl.innerHTML += `<div class="col-4 card" style="width: 20rem;height:30rem;"><div class="">
+      productsEl.innerHTML += `<div class="col-4 card" style="width: 20rem;height:30rem;"><div class="">
     <img src="${element.img_src}" class="card-img-top "></div>
     <div class="card-body color">
       <h5 class="card-title">${element.title}</h5>
       <p class="card-text">${element.desc}</p>
-      <button class="btnColor">ajouter au panier</button>
+      <button class="btnColor" data-ref="${element.ref}">ajouter au panier</button>
     </div>
   </div>`;
-   });
-})
+    });
+    addToCart();
+  })
 
 // function addToCart (ajouter au panier)
+const addToCart = () => {
+ 
+  let modalEl = document.getElementById("cart");
+  let btnList = Array.from(document.getElementsByClassName("btnColor"));
+
+  btnList.forEach(element => {
+    element.addEventListener("click", function (e) {
+      
+      let datatest = e.target.dataset.ref;
+      let testCart = false;
+      fetch('/assets/data/products.json')
+        .then((response) => response.json())
+        .then((displayCards) => {
+          displayCards.products.forEach((element) => {
+            if (datatest == element.ref) {
+              collectionCart.forEach(verif => {
+                if (verif == element.ref) {
+                  testCart = true;
+                }
+              });
+              if (testCart) {
+                let quantityTest = document.getElementById(element.ref)
+                quantityTest.innerHTML++;
+              }else {
+                if (nullCart) {
+                  modalEl.innerHTML = "";
+                  nullCart = false;
+                }
+                modalEl.innerHTML += `
+                <div class="d-flex cartElement w-100" data-ref="${element.ref}">
+                  <img src="${element.img_src}" width="50px">
+                  <p class="mr-5"> ${element.title} <br> ${element.price} € / KG </p>
+                  <div class="d-flex ml-5">
+                    <p>quantité : <span id="${element.ref}">1</span></p>
+                    <button> + </button>
+                  </div>
+                  
+                </div>
+                `;
+                collectionCart.push(element.ref);
+              }
+            }
+          });
+        })
+    });
+  });
+}
