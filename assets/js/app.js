22a0fc2d8cff90d949eb26b02c73ff6dda1d1387
paulinegallezot <paulinegallezot@gmail.com>
2021-05-25 14:55:22 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,7 +58,7 @@ btnHorse.addEventListener('click',function(){
 }) 
 
 const displayCard = (element)=>{
-  let card = `<div class="col-4 card mt-4 mb-4" style="width: 20rem;height:32rem;"><div class="">
+  let card = `<div class="col-4 card ml-2 mt-4 mb-4" style="width: 20rem;height:32rem;"><div class="">
     <img src="${element.img_src}" class="card-img-top "></div>
     <div class="card-body color">
       <h5 class="card-title">${element.title}</h5>
